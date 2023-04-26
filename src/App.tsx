9a2a6b93527d950e4346c43ad338d5ed1c55773c
Hélio Schneider <helioschndr@gmail.com>
2023-04-26 17:10:50 -0300

    Version - 0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import * as C from './App.styles'
+import { useState } from 'react';
+import React from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
 
 const App = () => {
+
+  const [list, setList] = useState(items)
+  const [currentMonth, setCurrentMonth] = useState();
+
   return (
     <C.Container>
-      ...
+      <C.Header>
+        <C.HeaderText>
+          <h1>Sistema Financeiro</h1>
+        </C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+        {/*Information Area*/}
+
+        {/* Insert Area*/}
+
+        {/*Item Table*/}
+
+      </C.Body>
+      
     </C.Container>
   );
 }
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2021, 9, 15), category: 'food', title: 'BurguerKing', value: 23},
+    { date: new Date(2021, 9, 15), category: 'rent', title: 'Aluguel', value: 1200},
+    { date: new Date(2021, 9, 15), category: 'salary', title: 'Meta', value: 4000}
+]
