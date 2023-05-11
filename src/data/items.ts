b485581929773fc97854c189271248d1d3f4e058
Hélio Schneider <helioschndr@gmail.com>
2023-05-11 18:12:29 -0300

    Update items.ts

--- src/data/items.ts
+++ src/data/items.ts
@@ -4,5 +4,6 @@ export const items: Item[] = [
     { date: new Date(2023, 4, 2), category: 'food', title: 'McDonalds', value: 32.12},
     { date: new Date(2023, 4, 2), category: 'food', title: 'BurguerKing', value: 23},
     { date: new Date(2023, 4, 2), category: 'rent', title: 'Aluguel', value: 1200},
-    { date: new Date(2023, 4, 2), category: 'salary', title: 'Meta', value: 4000}
+    { date: new Date(2023, 4, 2), category: 'salary', title: 'Meta', value: 4000},
+    { date: new Date(2023, 4, 2), category: 'salary', title: 'Freelance', value: 500}
 ]
